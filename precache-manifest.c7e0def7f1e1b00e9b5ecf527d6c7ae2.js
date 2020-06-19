f77456030ca2772a4d32c3cc27059d291ee8118b
timazubar <timazubar33@gmail.com>
2020-06-19 17:33:48 +0300

    Updates

---
+++ precache-manifest.c7e0def7f1e1b00e9b5ecf527d6c7ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa24e636a43a27230abf35472129b6d",
+    "url": "/redux-bookstore/index.html"
+  },
+  {
+    "revision": "657094dadd0a4da807c1",
+    "url": "/redux-bookstore/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "657094dadd0a4da807c1",
+    "url": "/redux-bookstore/static/js/2.c311c879.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/redux-bookstore/static/js/2.c311c879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc768fa21d2d788d08f7",
+    "url": "/redux-bookstore/static/js/main.d9d89166.chunk.js"
+  },
+  {
+    "revision": "cfeb627733f410295c84",
+    "url": "/redux-bookstore/static/js/runtime-main.f70708fe.js"
+  }
+]);
